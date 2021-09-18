50b0dc7b0b01b0d23ea44541cc6144cb52870cbd
Elise Zheng <elise.zheng@hotmail.fr>
2021-09-18 02:03:05 -0400

    1.b

--- glsl/remote.fs.glsl
+++ glsl/remote.fs.glsl
@@ -1,7 +1,6 @@
-uniform int tvChannel;
-void main() {
-	// HINT: WORK WITH tvChannel HERE
+uniform vec3 remotePosition;
 
-	//Paint it red
-	gl_FragColor = vec4(1, 0, 0, 1);
+void main() {
+
+	gl_FragColor = vec4(0, 0, remotePosition.y, 1);
 }
